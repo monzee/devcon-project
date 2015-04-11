e07e8704abf153f13c22dc1439d7b5e83e4bbe64
Mon Zafra <mon@gondar>
2015-04-11 17:07:34 +0800

    implemented contact form + saving + reviewing sent messages. done for now.

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import flask
+import time
 
 app = flask.Flask(__name__)
+app.secret_key = 'super_secret'
+
+MESSAGES = []
 
 @app.route('/')
 def home():
@@ -12,11 +16,25 @@ def about():
 
 @app.route('/contact', methods=['GET'])
 def contact():
-  return flask.render_template('contact.html')
+  flash = flask.get_flashed_messages()
+  if len(flash) > 0:
+    messages = flash
+  else:
+    messages = None
+  return flask.render_template('contact.html', messages=messages)
 
 @app.route('/contact', methods=['POST'])
 def after_contact():
-  pass
+  timestamp = time.strftime('%y-%b-%d %I:%M%p', time.localtime())
+  name = flask.request.form.get('name')
+  message = flask.request.form.get('message')
+  MESSAGES.append((timestamp, name, message))
+  flask.flash('Thank you for dropping by!')
+  return flask.redirect(flask.url_for('contact'))
+
+@app.route('/messages')
+def messages():
+  return flask.render_template('messages.html', xs=MESSAGES)
 
 if __name__ == '__main__':
   app.run(host='0', debug=True)
